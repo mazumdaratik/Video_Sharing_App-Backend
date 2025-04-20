33b298952f925dc735885b5a443dae5b411e9dd4
mazumdaratik <rjatik990@gmail.com>
2025-04-20 22:56:49 +0600

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express"; // <-- Import express
-
-const app = express(); // <-- Create app instance
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 });
 
 connectDB()
